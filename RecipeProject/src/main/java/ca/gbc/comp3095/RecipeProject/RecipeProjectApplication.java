b7886946a672fc5962e5d1dd7880893fc268597e
Nate Power <power2677247@gmail.com>
2021-10-14 13:57:15 -0700

    commit of Spring Boot framework

---
+++ RecipeProject/src/main/java/ca/gbc/comp3095/RecipeProject/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.RecipeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+
+}
