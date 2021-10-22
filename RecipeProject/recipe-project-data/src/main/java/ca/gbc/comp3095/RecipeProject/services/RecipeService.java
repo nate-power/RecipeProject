3892dff60540b4abd512ea69e19c657edf7b07de
justinbartlett9 <coldmilk19@gmail.com>
2021-10-22 19:22:50 -0400

    Created CrudService and RecipeService interfaces, RecipeController, and two recipe templates.

---
+++ RecipeProject/recipe-project-data/src/main/java/ca/gbc/comp3095/RecipeProject/services/RecipeService.java
@@ -0,0 +1,9 @@
+package ca.gbc.comp3095.RecipeProject.services;
+
+import ca.gbc.comp3095.RecipeProject.model.Recipe;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RecipeService extends CrudService<Recipe, Long>{
+
+}
--- RecipeProject/recipe-project-web/src/main/java/ca/gbc/comp3095/RecipeProject/controllers/RecipeController.java
+++ RecipeProject/recipe-project-web/src/main/java/ca/gbc/comp3095/RecipeProject/controllers/RecipeController.java
@@ -1,7 +1,32 @@
 package ca.gbc.comp3095.RecipeProject.controllers;
 
+import ca.gbc.comp3095.RecipeProject.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
+
+    public final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping({"/recipes", "/recipes/"} )
+    public String listRecipes() {
+        return "recipes/index";
+    }
+
+    @GetMapping({"/recipe/add", "/recipe/add/"} )
+    public String createRecipe() {
+        return "recipes/add";
+    }
+
+    @PostMapping({"/recipe/add", "/recipe/add/"} )
+    public String saveRecipe() {
+
+        return "recipes/index";
+    }
 }
