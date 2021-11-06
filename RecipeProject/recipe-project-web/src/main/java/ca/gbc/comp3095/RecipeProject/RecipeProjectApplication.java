c64c6320213225d895a95416f3bf3a6ec311f0c0
Nate Power <power2677247@gmail.com>
2021-11-06 14:54:03 -0700

    fixed weekly meals Saturday and Sunday determination

--- RecipeProject/recipe-project-web/src/main/java/ca/gbc/comp3095/RecipeProject/RecipeProjectApplication.java
+++ RecipeProject/recipe-project-web/src/main/java/ca/gbc/comp3095/RecipeProject/RecipeProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeProjectApplication {
 
+	// connect to http://localhost:8049/
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeProjectApplication.class, args);
 	}
